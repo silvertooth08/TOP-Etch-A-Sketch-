ded435b883326dcce04a2b391d643a2a478565ba
silvertooth08 <silv3rtooth@gmail.com>
2022-09-30 17:39:20 +0300

    Added buttons and fixed positioning

--- main.js
+++ main.js
@@ -4,19 +4,41 @@ function createGrid(gridSize) {
     const containerDiv = document.createElement('div');
     containerDiv.style.setProperty('--col-num', gridSize);
     containerDiv.classList.add('container');
-    body.appendChild(containerDiv);
+    document.body.appendChild(containerDiv);
 
 
 
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
-            const divs = document.createElement('div')
-           containerDiv.appendChild(divs)
+            const divs = document.createElement('div');
+           containerDiv.appendChild(divs);
         }
     }
     return containerDiv;
 }
 
 
-const containerDiv = createGrid(16);
+const containerDiv = createGrid(2);
 
+
+const buttonContainer = document.createElement('div')
+buttonContainer.classList.add('button-container'); 
+document.body.appendChild(buttonContainer);
+
+
+const colorButton = document.createElement('button');
+colorButton.innerText = 'Color Picker';
+buttonContainer.appendChild(colorButton);
+
+
+const rainbowButton = document.createElement('button');
+rainbowButton.innerText = 'Rainbow Effect';
+buttonContainer.appendChild(rainbowButton);
+
+const eraserButton = document.createElement('button');
+eraserButton.innerText = 'Eraser';
+buttonContainer.appendChild(eraserButton);
+
+const clearButton = document.createElement('button');
+clearButton.innerText = 'Clear';
+buttonContainer.appendChild(clearButton);
