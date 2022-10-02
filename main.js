0476b515129c191605c52fa45cf8bbf54adece51
silvertooth08 <silv3rtooth@gmail.com>
2022-10-02 19:05:38 +0300

    adjusting div size fixed

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
 "use strict";
 
+const containerDiv = document.createElement('div');
+containerDiv.classList.add('container');
+const body = document.querySelector('body')
+document.body.appendChild(containerDiv);
 
 function createGrid(gridSize) {
-const body = document.querySelector('body')
-    const containerDiv = document.createElement('div');
     containerDiv.style.setProperty('--col-num', gridSize);
-    containerDiv.classList.add('container');
-    document.body.appendChild(containerDiv);
 
 
-
+    
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
             const divs = document.createElement('div');
-           containerDiv.appendChild(divs);
+            containerDiv.appendChild(divs);
         }
     }
     return containerDiv;
 }
 
-let containerDiv = createGrid(16);
+const gridSizeRange = document.createElement('input');
+gridSizeRange.setAttribute('type','range');
+gridSizeRange.max = 100;
+gridSizeRange.min = 1;
+gridSizeRange.defaultValue = 16;
+gridSizeRange.addEventListener('input',updateGridInput);
+gridSizeRange.addEventListener('input',deleteGrid);
+gridSizeRange.addEventListener('input',updateCreateGrid);;
+gridSizeRange.innerText = gridSizeRange.value;
+
+let createdDivs = createGrid(gridSizeRange.value) 
 
 const buttonContainer = document.createElement('div')
 buttonContainer.classList.add('button-container'); 
@@ -29,31 +39,36 @@ const rangeContainer = document.createElement('div');
 rangeContainer.classList.add('range-container');
 buttonContainer.appendChild(rangeContainer);
 
-const gridSizeRange = document.createElement('input');
-gridSizeRange.setAttribute('type','range');
-gridSizeRange.max = 100;
-gridSizeRange.min = 1;
-gridSizeRange.defaultValue = 16;
-gridSizeRange.addEventListener('input',updateGridInput)
-gridSizeRange.innerText = gridSizeRange.value;
 rangeContainer.appendChild(gridSizeRange);
 
 const gridDisplay = document.createElement('span');
 gridDisplay.id = 'gridDisplay';
-gridDisplay.innerText = 16;
-rangeContainer.appendChild(gridDisplay)
+gridDisplay.innerText = gridSizeRange.value;
+rangeContainer.appendChild(gridDisplay);
 
 
-function updateGridInput(val) {
-    let output = document.getElementById('gridDisplay');
-  return  output.innerText = gridSizeRange.value;
-  }
+function updateGridInput() {
+    gridDisplay.innerText = gridSizeRange.value;
+}
+
+function deleteGrid (){
+    while (containerDiv.firstChild) {
+        containerDiv.firstChild.remove()
+    }
+}
+
+function updateCreateGrid () {
+ createGrid(gridSizeRange.value);
+}
 
 const colorButton = document.createElement('button');
 colorButton.innerText = 'Color Picker';
 buttonContainer.appendChild(colorButton);
 
 // Color picking function 
+function pickColor () {
+
+}
 
 
 const rainbowButton = document.createElement('button');
@@ -62,11 +77,19 @@ buttonContainer.appendChild(rainbowButton);
 
 // Random Color picking function with %10 contrast added every pass on the same div
 
+function pickRandomColor(){
+
+}
+
 const eraserButton = document.createElement('button');
 eraserButton.innerText = 'Eraser';
 buttonContainer.appendChild(eraserButton);
 
 // Erasing divs button (making them the initial background color,e.g. White)
+
+function eraseSketch () {
+
+}
 
 
 
@@ -75,5 +98,11 @@ clearButton.innerText = 'Clear';
 buttonContainer.appendChild(clearButton);
 
 // Clearing all divs button (making them the initial background color,e.g. White)
+
+function clearSketch () {
+    
+}
+
+
 
 
